ff0ff56b838baf43d41f3d9c6dd644e7a54d82b4
mscherer <mark.scherer@spryker.com>
2016-11-21 12:12:40 +0100

    Add empty OmsDependencyProvider

---
+++ src/Pyz/Zed/Oms/OmsDependencyProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pyz\Zed\Oms;
+
+use Spryker\Zed\Oms\OmsDependencyProvider as SprykerOmsDependencyProvider;
+
+class OmsDependencyProvider extends SprykerOmsDependencyProvider
+{
+}
